b472be1e3061d0aed1d8d4a7203cc809a713a050
Matt Banasiak <mattbanasiak@hotmail.com>
2013-11-05 12:04:34 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Opeagle::Application.config.secret_token = '6bb4d5aef68d5d01941144a0619e5f51b261e92f1e094d778d874e8043f30fab495cbf77eaebc6bca2aec98f185042c3a3146fe055e7c7bf311c18eac365b4a4'
